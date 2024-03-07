02f2230bee3ca68f7a10c4e55023815b597800f2
Moulali Syed <syedmoulali1997@gmail.com>
2024-03-07 09:15:22 +0530

    basic page layouts and navigation setup

--- Lesson4/config/allowedOrigins.js
+++ Lesson4/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ['http://localhost:3000', 'https://www.randomurl.com'];
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://www.randomurl.com',
+  'http://localhost:5173',
+];
 
 module.exports = allowedOrigins;
---
+++ Lesson5/src/features/auth/Login.jsx
@@ -0,0 +1,4 @@
+const Login = () => {
+  return <h1 style={{ color: '#fff' }}>Login</h1>;
+};
+export default Login;
